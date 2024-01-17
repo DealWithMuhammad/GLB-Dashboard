a70df4f179cfc4fce2abf15832102ca2512f1e9c
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-17 21:35:36 +0800

    feat: updated present and absent

--- components/Charts/ChartOne.tsx
+++ components/Charts/ChartOne.tsx
@@ -135,12 +135,12 @@ const ChartOne: React.FC = () => {
   const [state, setState] = useState<ChartOneState>({
     series: [
       {
-        name: "Absent",
+        name: "Present",
         data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
       },
 
       {
-        name: "Present",
+        name: "Absent",
         data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
       },
     ],
