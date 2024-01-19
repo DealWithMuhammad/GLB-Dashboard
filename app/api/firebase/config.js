c72c0c76853a8a01401d1447c8de9bafd76a68d5
bossisworking <157166019+bossisworking@users.noreply.github.com>
2024-01-19 21:27:49 +0000

    authentication

---
+++ app/api/firebase/config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGh_liL_9Hm2cdjm3IsAakBM22OkZvGmM",
+    authDomain: "glbdirge.firebaseapp.com",
+    projectId: "glbdirge",
+    storageBucket: "glbdirge.appspot.com",
+    messagingSenderId: "256564045951",
+    appId: "1:256564045951:web:2d1bd4dd1a25a8c13bf74a",
+    measurementId: "G-CQZHW1YEMH"
+};
+
+export const app = initializeApp(firebaseConfig);
