1da2d271986caf92c9c882120de3412684778290
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-24 14:56:46 +0800

    feat: add address form

--- components/StudentForm/AddStudent.tsx
+++ components/StudentForm/AddStudent.tsx
@@ -91,6 +91,17 @@ export default function AddStudent() {
                   />
                 </div>
 
+                <div className="mb-4.5 ">
+                  <label className="mb-2.5 block text-black dark:text-white">
+                    Address
+                  </label>
+                  <input
+                    type="phone"
+                    placeholder="Enter Student's Address"
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                  />
+                </div>
+
                 <div className="mb-4.5">
                   <label className="mb-2.5 block text-black dark:text-white">
                     Grade
