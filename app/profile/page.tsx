bafa0fabf5b90b04935a9ecc200a7e04f958dbe4
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-26 11:23:54 +0800

    feat: last commit of AddStudent

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Image from "next/image";
-import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
 
 import { Metadata } from "next";
 import React from "react";
@@ -12,20 +11,10 @@ export const metadata: Metadata = {
 };
 
 const Profile = () => {
-  const [value, setValue] = React.useState("1");
-
   return (
     <>
       <Breadcrumb pageName="Profile" />
       <div>Profile of person</div>
-
-      <RadioGroup onChange={setValue} value={value}>
-        <Stack direction="row">
-          <Radio value="1">First</Radio>
-          <Radio value="2">Second</Radio>
-          <Radio value="3">Third</Radio>
-        </Stack>
-      </RadioGroup>
     </>
   );
 };
--- components/StudentForm/AddStudent.tsx
+++ components/StudentForm/AddStudent.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import React from "react";
+
 export default function AddStudent() {
+  const [value, setValue] = React.useState("1");
   return (
     <>
       <div className="grid grid-cols-1 ">
@@ -52,78 +58,38 @@ export default function AddStudent() {
                     <label className="mb-2.5 block text-black dark:text-white">
                       Gender
                     </label>
+
+                    <RadioGroup onChange={setValue} value={value}>
+                      <Stack direction="row">
+                        <Radio value="1">Male</Radio>
+                        <Radio value="2">Female</Radio>
+                      </Stack>
+                    </RadioGroup>
+                  </div>
+                </div>
+
+                <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                  <div className="w-full xl:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                      Address
+                    </label>
                     <input
-                      type="radio"
-                      placeholder="Enter Student's last name"
+                      type="text"
+                      placeholder="Enter Student's Address"
                       className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     />
                   </div>
-                </div>
 
-                <div className="mb-4.5">
-                  <label className="mb-2.5 block text-black dark:text-white">
-                    Gender
-                  </label>
-                  <div className="relative z-20 bg-transparent dark:bg-form-input">
-                    <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
-                      <option value="">Gender</option>
-                      <option value="">Male</option>
-                      <option value="">Female</option>
-                      <option value="">Other</option>
-                    </select>
-                    <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
-                      <svg
-                        className="fill-current"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g opacity="0.8">
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                            fill=""
-                          ></path>
-                        </g>
-                      </svg>
-                    </span>
+                  <div className="w-full xl:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                      Phone Number
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter Student's Phone Number"
+                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                    />
                   </div>
-                </div>
-
-                <div className="mb-4.5 ">
-                  <label className="mb-2.5 block text-black dark:text-white">
-                    Father's Name
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Enter Student's Father's name"
-                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                  />
-                </div>
-
-                <div className="mb-4.5 ">
-                  <label className="mb-2.5 block text-black dark:text-white">
-                    Phone Number
-                  </label>
-                  <input
-                    type="phone"
-                    placeholder="Enter Student's Phone Number"
-                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                  />
-                </div>
-
-                <div className="mb-4.5 ">
-                  <label className="mb-2.5 block text-black dark:text-white">
-                    Address
-                  </label>
-                  <input
-                    type="phone"
-                    placeholder="Enter Student's Address"
-                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                  />
                 </div>
 
                 <div className="mb-4.5">
